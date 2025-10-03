7515346b56c1f0e3df6a0a9ddfe82dd4f14b14ae
tiffanyyvo <tiffanyvo20@yahoo.com>
2025-10-02 20:08:00 -0400

    Fixed the rounding issue, essay average, and the cimouting average

--- grade-calculator/grade_calculator.go
+++ grade-calculator/grade_calculator.go
@@ -1,5 +1,7 @@
 package esepunittests
 
+import "math"
+
 type GradeCalculator struct {
 	assignments []Grade
 	exams       []Grade
@@ -80,18 +82,21 @@ func (gc *GradeCalculator) AddGrade(name string, grade int, gradeType GradeType)
 func (gc *GradeCalculator) calculateNumericalGrade() int {
 	assignment_average := computeAverage(gc.assignments)
 	exam_average := computeAverage(gc.exams)
-	essay_average := computeAverage(gc.exams)
+	essay_average := computeAverage(gc.essays)
 
 	weighted_grade := float64(assignment_average)*.5 + float64(exam_average)*.35 + float64(essay_average)*.15
 
-	return int(weighted_grade)
+	return int(math.Round(weighted_grade))
 }
 
 func computeAverage(grades []Grade) int {
+	if len(grades) == 0 {
+		return 0
+	}
 	sum := 0
 
-	for grade, _ := range grades {
-		sum += grade
+	for _, g := range grades {
+		sum += g.Grade
 	}
 
 	return sum / len(grades)
